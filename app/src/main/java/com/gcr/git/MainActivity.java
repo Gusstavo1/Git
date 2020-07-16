fc31a5821c1911c763bd15e2c2eacbdbc69c555d
Gusstavo1 <ipngustavo@gmail.com>
2020-07-16 09:26:00 -0500

    Primer commit

---
+++ app/src/main/java/com/gcr/git/MainActivity.java
@@ -0,0 +1,19 @@
+package com.gcr.git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"Master");
+    }
+}
