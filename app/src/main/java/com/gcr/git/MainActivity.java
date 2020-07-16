115711ed765db9065d6da7bd03c0d42e1dbd4dfc
Gusstavo1 <ipngustavo@gmail.com>
2020-07-16 09:28:11 -0500

    log

--- app/src/main/java/com/gcr/git/MainActivity.java
+++ app/src/main/java/com/gcr/git/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG,"Master");
+        Log.d(TAG,"Rama1");
     }
 }
