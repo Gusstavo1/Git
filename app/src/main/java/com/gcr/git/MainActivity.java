2550a3b1d7d51358ef9c95389c5f7a26c165bdaf
Gustavo <ipngustavo@gmail.com>
2020-08-14 09:25:47 -0500

    branch master logs messages

--- app/src/main/java/com/gcr/git/MainActivity.java
+++ app/src/main/java/com/gcr/git/MainActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
     private final static String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"bienvenido a Git");
+        Log.d(TAG,"bienvenido a Git");
+        Log.d(TAG,"bienvenido a Git");
+        Log.d(TAG,"bienvenido a Git");
+        Log.d(TAG,"bienvenido a Git");
+        Log.d(TAG,"bienvenido a Git");
+        Log.d(TAG,"bienvenido a Git");
+        Log.d(TAG,"bienvenido a Git");
     }
 }
